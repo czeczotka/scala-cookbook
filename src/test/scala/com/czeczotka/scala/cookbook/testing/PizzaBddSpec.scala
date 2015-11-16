d630c408e6780534cc30dbcabc5e168047bdbb56
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-11-16 18:57:37 +0100

    Testing, recipe 3 - Add sample BDD test

---
+++ src/test/scala/com/czeczotka/scala/cookbook/testing/PizzaBddSpec.scala
@@ -0,0 +1,25 @@
+package com.czeczotka.scala.cookbook.testing
+
+import org.scalatest.{BeforeAndAfter, FunSpec}
+
+class PizzaBddSpec extends FunSpec with BeforeAndAfter {
+
+  var pizza: Pizza = _
+
+  before {
+    pizza = new Pizza
+  }
+
+  describe("A Pizza") {
+
+    it("should start with no toppings") {
+      assert(pizza.getToppings.isEmpty)
+    }
+
+    it("should allow addition of toppings") (pending)
+
+    it("should allow removal of toppings") (pending)
+
+  }
+
+}
