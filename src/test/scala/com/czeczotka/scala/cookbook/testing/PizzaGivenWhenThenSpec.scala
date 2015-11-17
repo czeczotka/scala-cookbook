6d11c8b968d1d15dfbf9b6116344e687f4a6515e
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-11-17 21:53:53 +0100

    Testing, recipe 4 - Add more Given When Then tests

--- src/test/scala/com/czeczotka/scala/cookbook/testing/PizzaGivenWhenThenSpec.scala
+++ src/test/scala/com/czeczotka/scala/cookbook/testing/PizzaGivenWhenThenSpec.scala
@@ -8,6 +8,14 @@ class PizzaGivenWhenThenSpec extends FunSpec with GivenWhenThen {
 
   describe("A Pizza") {
 
+    it("should start with no toppings") {
+      Given("a new pizza")
+      pizza = new Pizza
+
+      Then("the topping count should be zero")
+      assertResult(0)(pizza.getToppings.size)
+    }
+
     it("should allow the addition of toppings") {
       Given("a new pizza")
       pizza = new Pizza
@@ -22,6 +30,18 @@ class PizzaGivenWhenThenSpec extends FunSpec with GivenWhenThen {
 
       And("the topping should be what was added")
       assert(pizza.getToppings.head == Topping("green olives"))
+    }
+
+    it("should allow removal of toppings") {
+      Given("a new pizza with one topping")
+      pizza = new Pizza
+      pizza.addTopping(Topping("green olives"))
+
+      When("the topping is removed")
+      pizza.removeTopping(Topping("green olives"))
+
+      Then("the topping count should be zero")
+      assertResult(0)(pizza.getToppings.size)
     }
   }
 }
