e2ec50868a5163f91377b6ee2810ee4560fc63ee
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-11-25 21:57:43 +0000

    Testing, recipe 12 - Java-Style Logging with SLF4J

--- build.sbt
+++ build.sbt
@@ -6,3 +6,6 @@ scalaVersion := "2.11.7"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % Test withSources()
 libraryDependencies += "org.mockito" % "mockito-all" % "1.10.19" % Test withSources()
+
+libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.12"
+libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.7"
--- src/main/scala/com/czeczotka/scala/cookbook/testing/Pizza.scala
+++ src/main/scala/com/czeczotka/scala/cookbook/testing/Pizza.scala
@@ -1,16 +1,23 @@
 package com.czeczotka.scala.cookbook.testing
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import scala.collection.mutable.ArrayBuffer
 
 class Pizza {
 
   private val toppings = new ArrayBuffer[Topping]
+  private val logger = LoggerFactory.getLogger(classOf[Pizza])
+
+  logger.info("Creating a new pizza")
 
   def addTopping(t: Topping) {
+    logger.info(s"Adding $t")
     toppings += t
   }
 
   def removeTopping(t: Topping) {
+    logger.info(s"Removing $t")
     toppings -= t
   }
 
